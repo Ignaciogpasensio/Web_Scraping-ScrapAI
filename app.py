071dcb77bfdcd9f9ac25abfed44eb9df80dc5f60
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-01 20:32:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import streamlit as st
 import json
 import subprocess
 
-def run_find_script(category):
-    # Run the find.py script with the selected category
+def run_scrap_script(category):
+    # Run the scrap.py script with the selected category
     result = subprocess.run(['python3', 'scrap.py', category], capture_output=True, text=True)
     if result.returncode != 0:
-        st.error("Error running scrap.py script")
+        st.error(f"Error running scrap.py script: {result.stderr}")
         st.stop()
 
 def display_results():
@@ -25,6 +25,6 @@ st.title('Scalpers Category Finder')
 category = st.selectbox('Select a category:', ['jeans', 'shirts'])
 
 if st.button('Find'):
-    run_find_script(category)
+    run_scrap_script(category)
     st.success(f"Results for {category} have been saved to find.json")
     display_results()
