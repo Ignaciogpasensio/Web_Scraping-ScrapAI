0f73dc710e2640c203380459d6b3e23053819447
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-02 16:33:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ def main():
         data = load_data(subcategory)
 
         # Create columns for product display
-        cols = st.columns(4)
+        cols = st.columns(6)
         for index, product in enumerate(data):
             discount_text = f"-{product['product_discount']}%"
             image_url = product['product_image_url']
@@ -252,7 +252,7 @@ def main():
 
             # Filter products based on price and discount range
             if min_price <= product_price_after <= max_price and min_discount <= product['product_discount'] <= max_discount:
-               cols[index % 4].markdown(f"""
+               cols[index % 6].markdown(f"""
                 <a href="{product_page_url}" target="_blank" style="text-decoration: none; color: inherit;">
                     <div class="product-container">
                         <div class="tooltip">
