91bfa280946adc125f96bd43e3471b3945b63cb7
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-04 01:43:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import subprocess
 import json
 
-# Function to run scrap.py with arguments
+# Función para ejecutar scrap.py con argumentos
 def run_scraping(category, min_price, max_price, min_discount, max_discount):
     command = ['python', 'scrap.py', '--category', category]
 
@@ -17,20 +17,20 @@ def run_scraping(category, min_price, max_price, min_discount, max_discount):
 
     subprocess.run(command)
 
-# Function to load and format JSON data
+# Función para cargar y formatear datos JSON
 def load_data(category):
     filename = f'search.json'
     with open(filename, 'r', encoding='utf-8') as f:
         data = json.load(f)
-    
-    # Iterate through products and format sizes and colors
+
+    # Iterar a través de los productos y formatear tamaños y colores
     for product in data:
         product['sizes'] = '/'.join(product['sizes'])
         product['colors'] = '/'.join(product['colors'])
-    
+
     return data
 
-# Mapping dictionary for subcategory display names
+# Diccionario de mapeo para nombres de subcategoría
 subcategory_names = {
     'vestidos_monos': 'Vestidos & Monos',
     'faldas': 'Faldas',
@@ -75,7 +75,7 @@ subcategory_names = {
     'fragancias': 'Fragancias'
 }
 
-# Streamlit app
+# Aplicación Streamlit
 def main():
     categories = {
         'Ropa': ['vestidos_monos', 'faldas', 'camisas', 'camisetas', 'tops', 'sudaderas', 'brazers_chalecos', 'pantalones', 'jeans', 'bermudas_shorts', 'chaquetas_trench', 'jerseis_cardigan', 'punto', 'total_look', 'pijamas', 'bikinis_bañadores', 'athleisure'],
@@ -89,18 +89,18 @@ def main():
     .sidebar .sidebar-content {
         background-color: white !important;
         color: black !important;
-        padding: 20px; /* Adds padding inside the sidebar */
-        border-right: 2px solid #ccc; /* Adds a border on the right side of the sidebar */
+        padding: 20px; /* Añade padding dentro de la barra lateral */
+        border-right: 2px solid #ccc; /* Añade un borde en el lado derecho de la barra lateral */
     }
     .sidebar select {
         background-color: white !important;
         color: black !important;
-        border: 1px solid #ccc !important; /* Adjust border color and style as needed */
+        border: 1px solid #ccc !important; /* Ajusta el color y estilo del borde según sea necesario */
         border-radius: 4px;
         padding: 8px;
         font-size: 14px;
-        width: 100%; /* Optional: Adjust width to fit your layout */
-        box-shadow: none !important; /* Optional: Remove box shadow */
+        width: 100%; /* Opcional: Ajusta el ancho para que se ajuste a tu diseño */
+        box-shadow: none !important; /* Opcional: Elimina la sombra */
     }
     .sidebar .stButton {
         background-color: #007bff !important;
@@ -114,7 +114,7 @@ def main():
         background-color: #0056b3 !important;
         border-color: #0056b3 !important;
     }
-        .title-container {
+    .title-container {
         background-image: url('https://i.pinimg.com/736x/c0/51/e0/c051e01026300529d49ce029aef829dc.jpg'); /* URL de tu imagen de fondo */
         background-size: cover;
         background-position: center;
@@ -126,22 +126,22 @@ def main():
         font-family: 'Arial', sans-serif;
         font-size: 80px;
         font-weight: bold;
-        color: black;
-        text-align: center; /* Center align the title */
+        color: white;
+        text-align: center; /* Centra el título */
         margin-top: 0px;
-        margin-bottom: 20px; /* Optional: Add some bottom margin */
+        margin-bottom: 20px; /* Opcional: Añade un margen inferior */
     }
     .product-container {
         position: relative;
         display: inline-block;
         width: 100%;
-        margin-bottom: 20px; /* Adjust spacing between products */
+        margin-bottom: 20px; /* Ajusta el espacio entre productos */
     }
     .product-container img {
         width: 100%;
         height: auto;
         transition: transform 0.2s, opacity 0.2s;
-        position: relative; /* Ensure discount text is positioned relative to the image */
+        position: relative; /* Asegura que el texto de descuento esté posicionado relativo a la imagen */
     }
     .product-container:hover img {
         transform: scale(1.05);
@@ -149,18 +149,18 @@ def main():
     }
     .discount-text {
         position: absolute;
-        top: 50%; /* Center discount text vertically */
-        left: 50%; /* Center discount text horizontally */
-        transform: translate(-50%, -50%); /* Adjust for centering */
-        color: black; /* Text color */
-        font-size: 4em; /* Double size: 2em was previous size */
+        top: 50%; /* Centra verticalmente el texto de descuento */
+        left: 50%; /* Centra horizontalmente el texto de descuento */
+        transform: translate(-50%, -50%); /* Ajusta para centrar */
+        color: black; /* Color del texto */
+        font-size: 4em; /* Doble tamaño: 2em era el tamaño anterior */
         font-weight: bold;
         z-index: 1;
     }
     .tooltip {
         position: relative;
         display: inline-block;
-        z-index: 0; /* Ensure tooltips are behind the discount text */
+        z-index: 0; /* Asegura que los tooltips estén detrás del texto de descuento */
     }
     .tooltip .tooltiptext {
         visibility: hidden;
@@ -172,31 +172,26 @@ def main():
         padding: 5px 0;
         border-radius: 6px;
         position: absolute;
-        z-index: 2; /* Ensure tooltips are above other elements */
+        z-index: 2; /* Asegura que los tooltips estén por encima de otros elementos */
         bottom: 125%;
         left: 50%;
         margin-left: -100px;
         opacity: 0;
         transition: opacity 0.3s;
-        pointer-events: none; /* Ensure tooltip does not interfere with mouse events */
+        pointer-events: none; /* Asegura que el tooltip no interfiera con los eventos del mouse */
     }
     .tooltip:hover .tooltiptext {
         visibility: visible;
         opacity: 1;
     }
     .tooltip .tooltiptext .smaller-text {
-        font-size: 0.7em; /* Adjust the font size smaller */
-        line-height: 1.5; /* Adjust the line height */
-    }
-    /* Background image styling */
-    .stApp {
-        background: url("fondo.jpg") no-repeat center center fixed;
-        background-size: cover;
+        font-size: 0.7em; /* Ajusta el tamaño de la fuente a más pequeño */
+        line-height: 1.5; /* Ajusta la altura de línea */
     }
     </style>
     """, unsafe_allow_html=True)
 
-    # JavaScript to adjust tooltip position dynamically
+    # JavaScript para ajustar la posición del tooltip dinámicamente
     st.markdown(
         """
         <script>
@@ -217,36 +212,36 @@ def main():
         unsafe_allow_html=True
     )
 
-    # Sidebar - Main Category selection
+    # Barra lateral - Selección de categoría principal
     main_category = st.sidebar.selectbox('Selecciona la Categoría', list(categories.keys()))
 
-    # Sidebar - Subcategory selection based on main category
+    # Barra lateral - Selección de subcategoría basada en la categoría principal
     subcategory = st.sidebar.selectbox(f'¿Qué gama de {main_category} desea?', categories[main_category])
 
-    # Price range slider
+    # Slider de rango de precios
     price_range = st.sidebar.slider('Seleccione el rango de precios que está dispuesto a pagar', min_value=0.0, max_value=2000.0, value=(0.0, 2000.0), step=1.0)
     min_price = price_range[0]
     max_price = price_range[1]
 
-    # Discount range slider
+    # Slider de rango de descuento
     discount_range = st.sidebar.slider('Seleccione el rango de descuento que le interesa', min_value=0, max_value=100, value=(0, 100), step=1)
     min_discount = discount_range[0]
     max_discount = discount_range[1]
 
-    # Custom title with font style and center alignment
-    st.markdown('<p class="title">ScrapAI</p>', unsafe_allow_html=True)
+    # Título personalizado con estilo de fuente y alineación central
+    st.markdown('<div class="title-container"><p class="title">ScrapAI</p></div>', unsafe_allow_html=True)
 
     if st.sidebar.button('SCRAPE'):
-        with st.spinner('Bichendo ofertas...'):
+        with st.spinner('Buscando ofertas...'):
             run_scraping(subcategory, min_price, max_price, min_discount, max_discount)
 
-    # Display scraped product data
+    # Mostrar datos de productos raspados
     if st.sidebar.checkbox('Mostrar productos'):
         st.subheader(f'{subcategory_names[subcategory]}')
         data = load_data(subcategory)
 
-        # Create columns for product display
-        cols = st.columns(4)
+        # Crear columnas para mostrar productos
+        cols = st.columns(3)
         for index, product in enumerate(data):
             discount_text = f"-{product['product_discount']}%"
             image_url = product['product_image_url']
@@ -260,9 +255,9 @@ def main():
             sizes = product['sizes']
             colors = product['colors']
 
-            # Filter products based on price and discount range
+            # Filtrar productos según el rango de precio y descuento
             if min_price <= product_price_after <= max_price and min_discount <= product['product_discount'] <= max_discount:
-               cols[index % 4].markdown(f"""
+                cols[index % 3].markdown(f"""
                 <a href="{product_page_url}" target="_blank" style="text-decoration: none; color: inherit;">
                     <div class="product-container">
                         <div class="tooltip">
@@ -274,9 +269,9 @@ def main():
                                 <strong>{product_name}</strong><br>
                                 <s>{product_price_before}€</s><br>
                                 <strong>{product_price_after}€</strong><br>
-                                <div class="smaller-text">Brand: {product_brand}<br>
-                                Sizes: {sizes}<br>
-                                Colors: {colors}<br>
+                                <div class="smaller-text">Marca: {product_brand}<br>
+                                Tallas: {sizes}<br>
+                                Colores: {colors}<br>
                                 ID: {product_id}</div>
                             </span>
                         </div>
