72adc23e521d9a1299394a56ed95b906b17bf680
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-04 13:09:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def main():
         padding: 100px 0; /* Ajusta el espacio alrededor del título */
     }
     .title {
-        font-family: 'Helvetica Neue';
+        font-family: 'Nicolas Cochin';
         font-size: 80px;
         font-weight: bold;
         color: white;
