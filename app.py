d413284b52425ff6183f5f6546454ddc49f17f1c
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-04 12:56:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def main():
     }
     .title {
         font-family: 'Arial', sans-serif;
-        font-size: 80px;
+        font-size: 150px;
         font-weight: bold;
         color: white;
         text-align: center; /* Centra el título */
