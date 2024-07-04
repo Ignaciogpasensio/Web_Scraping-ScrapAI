d8c1b7897edbcec055c08b93fc9789e261904d6f
Ignacio García de Parada Asensio <134919136+Ignaciogpasensio@users.noreply.github.com>
2024-07-04 13:07:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def main():
         padding: 100px 0; /* Ajusta el espacio alrededor del título */
     }
     .title {
-        font-family: 'Bodoni', sans-serif;
+        font-family: 'Bodoni';
         font-size: 80px;
         font-weight: bold;
         color: white;
